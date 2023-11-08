608f1389b56f3adc6320fc4ed897376a156c5ba1
uros <urosstanic25@gmail.com>
2023-11-08 23:58:35 +0100

    implemented uds component

--- PI1.py
+++ PI1.py
@@ -1,5 +1,7 @@
 
 import threading
+
+from components.uds import run_uds
 from settings import load_settings
 from components.dht import run_dht
 import time
@@ -19,8 +21,10 @@ if __name__ == "__main__":
     try:
         dht1_settings = settings['DHT1']
         dht2_settings = settings['DHT2']
+        uds1_settings = settings['DUS1']
         run_dht(dht1_settings, threads, stop_event)
         run_dht(dht2_settings,threads,stop_event)
+        run_uds(uds1_settings,threads,stop_event)
         while True:
             time.sleep(1)
 
