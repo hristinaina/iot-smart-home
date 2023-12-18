331c7e005ae86475738986f3aac31e3dc0b25e8c
Hristina Adamovic <hristinacina@gmail.com>
2023-12-18 12:14:20 +0100

    changed server functions

--- backend/server.py
+++ backend/server.py
@@ -21,9 +21,10 @@ mqtt_client = mqtt.Client()
 mqtt_client.connect("localhost", 1883, 60)
 mqtt_client.loop_start()
 
+
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("Temperature")
-    client.subscribe("Humidity")
+    client.subscribe("data/+")
+
 
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = lambda client, userdata, msg: save_to_db(json.loads(msg.payload.decode('utf-8')))
@@ -36,20 +37,9 @@ def save_to_db(data):
         .tag("simulated", data["simulated"])
         .tag("runs_on", data["runs_on"])
         .tag("name", data["name"])
-        .field("measurement", data["value"])
+        .field("value", data["value"])
     )
     write_api.write(bucket=bucket, org=org, record=point)
-
-
-# Route to store dummy data
-@app.route('/store_data', methods=['POST'])
-def store_data():
-    try:
-        data = request.get_json()
-        store_data(data)
-        return jsonify({"status": "success"})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
 
 
 def handle_influx_query(query):
@@ -65,23 +55,6 @@ def handle_influx_query(query):
         return jsonify({"status": "success", "data": container})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
-
-
-@app.route('/simple_query', methods=['GET'])
-def retrieve_simple_data():
-    query = f"""from(bucket: "{bucket}")
-    |> range(start: -10m)
-    |> filter(fn: (r) => r._measurement == "Humidity")"""
-    return handle_influx_query(query)
-
-
-@app.route('/aggregate_query', methods=['GET'])
-def retrieve_aggregate_data():
-    query = f"""from(bucket: "{bucket}")
-    |> range(start: -10m)
-    |> filter(fn: (r) => r._measurement == "Humidity")
-    |> mean()"""
-    return handle_influx_query(query)
 
 
 if __name__ == '__main__':
